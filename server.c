3a0c4545b3946c880ef1562b4a9daf362edfdbd5
Zakaria Boudraa <zakariaboudraa@Ordinateur-portable-de-Zakaria.local>
2025-12-01 20:52:56 -0500

    created server socket

--- server.c
+++ server.c
@@ -6,6 +6,26 @@
 #include <fcntl.h>
 #include <errno.h>
 
+const int MAX_CLIENTS = 4;
+FILE *CLIENTS[MAX_CLIENTS] = {0};
+
+void redistribute_message(int sender_i, char *buf) {
+    for (int i = 0; i < MAX_CLIENTS; i ++) {
+        if (CLIENTS[i] == NULL || i == sender_i) {
+            continue;
+        } else {
+            if (0 > fprintf(CLIENTS[i], "%s", buf)) {
+                fclose(CLIENTS[i]);
+                CLIENTS[i] == NULL;
+            } else if (EOF == fflush(CLIENTS[i])) {
+                fclose(CLIENTS[i]);
+                CLIENTS[i] == NULL;
+            }
+        }
+    }
+}
+
 int main(int argc, char *argv[]) {
+
 
 }
