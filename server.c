b912b00099a5603b9ffc029226ff79b6d5964b4a
Zakaria Boudraa <zakariaboudraa@Ordinateur-portable-de-Zakaria.local>
2025-12-01 20:07:50 -0500

    added libraries

--- server.c
+++ server.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
 
 int main(int argc, char *argv[]) {
 
