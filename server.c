43a5c3ee1bf31e19d9f8928bb6872f3e0183bd74
Zakaria Boudraa <zakariaboudraa@Ordinateur-portable-de-Zakaria.local>
2025-12-01 22:00:18 -0500

    implemented helper functions

--- server.c
+++ server.c
@@ -9,8 +9,83 @@
 const int MAX_CLIENTS = 4;
 FILE *CLIENTS[MAX_CLIENTS] = {0};
 
+void redistribute_message(int sender_index, char *buff) {
+   for (int i = 0; i < MAX_CLIENTS; i ++) {
+        if (CLIENTS[i] == NULL || i == sender_index) {
+            continue;
+        } else {
+            if (0 > fprintf(CLIENTS[i], "%s", buf)) {
+                fclose(CLIENTS[i]);
+                CLIENTS[i] = NULL;
+            } else if (EOF == fflush(CLIENTS[i])) {
+                fclose(CLIENTS[i]);
+                CLIENTS[i] = NULL;
+            }
+        }
+   }
+}
+
+int read_message(char *buf, size_t len, int client_index) {
+    if (fgets(buf, (int)len, CLIENTS[client_index]) == NULL) {
+        if (errno == EAGAIN || errno == EWOULDBLOCK) {
+            return -1;
+        } else {
+            fclose(CLIENTS[client_index]);
+            CLIENTS[client_index] == NULL;
+            return -1;
+        }
+    }
+    return 1;
+}
+
+void add_client(int server_fd) {
+    if ((client_fd = accept(server_fd, NULL, NULL)) == -1) {
+        if (errno == EAGAIN || errno == EWOULDBLOCK) {
+            return;
+        } else {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+    } else {
+        int nonblock = fcntl(client_fd, F_SETFLG, O_NONBLOCKING);
+        if (nonblock == -1) {
+            perror("fcntl");
+            close(client_fd);
+            return;
+        }
+
+        FILE *client = fdopen(client_fd, "r+");
+        if (client == NULL) {
+            perror("fdopen");
+            close(client_fd);
+            return;
+        }
+
+        for (int i = o; i < MAX_CLIENTS; i ++) {
+            if (CLIENTS[i] == NULL) {
+                CLIENTS[i] = client;
+                return;
+            }
+        }
+
+        fprintf(client, "%s", "Sorry, the server is full");
+        fflush(client);
+        fclose(client);
+        return;
+    }
+}
+
 int loop(int server_fd) {
-    
+    char buff[1024];
+
+    while (1) {
+        for (int i = 0; i < MAX_CLIENTS; i ++) {
+            if (CLIENTS[i] == NULL) {
+                continue;
+            }
+            
+        }
+    }
 }
 
 int main(int argc, char *argv[]) {
