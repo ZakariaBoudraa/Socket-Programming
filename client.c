7132a921abba463361b0b37d8802b9b9fea4ae1f
Zakaria Boudraa <zakariaboudraa@Ordinateur-portable-de-Zakaria.local>
2025-12-02 18:45:48 -0500

    added libraries for client.c

--- client.c
+++ client.c
@@ -1,3 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
+
+
 int main(int argc, char *argv[]) {
-    
+
 }
