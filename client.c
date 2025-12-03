65e9883c459870e4271099acb76ca67ab2da9ae0
Zakaria Boudraa <zakariaboudraa@Ordinateur-portable-de-Zakaria.local>
2025-12-02 19:10:01 -0500

    client.c socket creation and connection

--- client.c
+++ client.c
@@ -9,4 +9,38 @@
 
 int main(int argc, char *argv[]) {
 
+    struct sockaddr_in address;
+    int sock_fd;
+    char buff[1024];
+
+    sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock_fd < ) {
+        perror("socket");
+        return 1;
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = htonl(INADDR_LOOPBACK);   //127.0.0.1 aka localhost
+    address.sin_port = htons(7878);
+
+    if (connect(sock_fd, (struct sockaddr *)&address, sizeof(address)) == -1) {
+        perror("connect");
+        return 1;
+    }
+
+    int nonblock = fcntl(0, F_SETFLG, O_NONBLOCKING);                 // make stdin nonblocking
+    if (nonblock == -1) {
+        perror("fcntl");
+        return 1;
+    }
+
+    int nonblock_sock = fcntl(sock_fd, F_SETFLG, O_NONBLOCKING);     // make server file descriptor nonblocking
+    if (nonblock_sock == -1) {
+        perror("fcntl");
+        return 1;
+    }
+
+    FILE *server = fdopen(sock_fd, "r+");                            // convert socket file descriptor in server file
+    for (;;) {
+    }
 }
